4ae00b3885ab273598e45e7f10af80775e80352a
Jidcoo <1337753396@qq.com>
2024-04-09 23:54:11 +0800

    Add interface LeetcodeInvoker.

---
+++ src/main/java/io/github/jidcoo/opto/lcdb/enhancer/core/executor/ConstructorLeetcodeInvoker.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (C) 2024-2026 Jidcoo(https://github.com/jidcoo).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.jidcoo.opto.lcdb.enhancer.core.executor;
+
+import io.github.jidcoo.opto.lcdb.enhancer.base.LeetcodeInvoker;
+import io.github.jidcoo.opto.lcdb.enhancer.utils.AssertUtil;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * <p>ConstructorLeetcodeInvoker is an invoker
+ * used for {@link Constructor} type.</p>
+ *
+ * @author Jidcoo
+ * @see LeetcodeInvoker
+ * @since 1.0.1
+ */
+final class ConstructorLeetcodeInvoker implements LeetcodeInvoker {
+
+    /**
+     * The base constructor;
+     */
+    private final Constructor<?> constructor;
+
+    /**
+     * Create a ConstructorLeetcodeInvoker instance.
+     *
+     * @param constructor the base constructor.
+     */
+    ConstructorLeetcodeInvoker(Constructor<?> constructor) {
+        AssertUtil.nonNull(constructor, "The constructor cannot be null.");
+        this.constructor = constructor;
+    }
+
+
+    /**
+     * Get parameter count of this invoker.
+     *
+     * @return parameter count of this invoker.
+     */
+    @Override
+    public int getParameterCount() {
+        return this.constructor.getParameterCount();
+    }
+
+    /**
+     * Get name of this invoker.
+     *
+     * @return the invoker name.
+     */
+    @Override
+    public String getInvokerName() {
+        return this.constructor.getName();
+    }
+
+    /**
+     * Get parameter types array of this invoker.
+     *
+     * @return the parameter types array.
+     */
+    @Override
+    public Class<?>[] getParameterTypes() {
+        return this.constructor.getParameterTypes();
+    }
+
+    /**
+     * Get return type of this invoker.
+     *
+     * @return the return type.
+     */
+    @Override
+    public Class<?> getReturnType() {
+        return this.constructor.getDeclaringClass();
+    }
+
+    /**
+     * Invoke this invoker with args by invoker holder.
+     *
+     * @param object the object holding this invoker.
+     * @param args   the invoking args.
+     * @return the invoked result.
+     * @throws Throwable throw an exception when there is an error
+     *                   in the invoking process.
+     */
+    @Override
+    public Object invoke(Object object, Object... args) throws Throwable {
+        return this.constructor.newInstance(object, args);
+    }
+}
