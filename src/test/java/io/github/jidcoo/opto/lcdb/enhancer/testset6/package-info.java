b1ab9825e7e331c0ebd365eb7c7b8720dde42f34
Jidcoo <1337753396@qq.com>
2025-03-22 19:08:25 +0800

    fix bug & add test.

---
+++ src/test/java/io/github/jidcoo/opto/lcdb/enhancer/testset6/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Tests for {@link io.github.jidcoo.opto.lcdb.enhancer.LeetcodeJavaDebugEnhancer#getEnhancerPayload()}
+ *
+ * @author Jidcoo
+ */
+package io.github.jidcoo.opto.lcdb.enhancer.testset6;
